939c18c8500aa2d928961d32375f86456a6c98cf
Dominik Honnef <dominik@honnef.co>
2025-03-25 04:18:02 +0100

    curve: fix bug in stroke expansion

--- curve/fit.go
+++ curve/fit.go
@@ -406,7 +406,7 @@ func fitToBezPathRec(
 					return false, true
 				}
 			}
-			return yield(CubicTo(c.P1, c.P2, c.P3)), true
+			return yield(CubicTo(c.P1, c.P2, c.P3)), false
 		}
 	}
 	var t float64
